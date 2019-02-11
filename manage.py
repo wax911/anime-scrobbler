551844d2b1521afb5c71efc6bde768efabcdd63f
Maxwell <mxt.developer@gmail.com>
2019-02-11 11:01:25 +0200

    Main Runner

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 import argparse
-from anilist import HttpController
-import app, nyaa, plex
+from anilist import AniListController, MediaTitle
+from plex import PlexController
+from nyaa import NyaaController
 
 
 def __description() -> str:
@@ -19,7 +20,7 @@ def __usage() -> str:
 
 def __init_cli() -> argparse:
     parser = argparse.ArgumentParser(description=__description(), usage=__usage())
-    parser.add_argument('-t', '--test', default='anilist',
+    parser.add_argument('-t', '--test', default='nyaa',
                         help="test different modules")
     parser.add_argument('-r', '--run', default='app',
                         help="run the production module")
@@ -37,11 +38,16 @@ def __init_app(args: argparse) -> None:
     run = args.run
     if test is not None:
         if test == 'anilist':
-            HttpController().make_request()
+            AniListController().make_request()
         elif test == 'nyaa':
-            pass
+            NyaaController.search_for_show(MediaTitle(
+                english='The Rising of the Shield Hero',
+                romaji='Tate no Yuusha no Nariagari',
+                native='盾の勇者の成り上がり',
+                userPreferred='Tate no Yuusha no Nariagari'
+            ))
         elif test == 'plex':
-            pass
+            PlexController().find_all_by_title('Anime')
     elif run is not None:
         if run == 'app':
             pass
